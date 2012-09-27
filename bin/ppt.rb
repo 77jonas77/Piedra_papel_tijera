3bafb10d52d0b135677c503dcf3f8cf849823cb6
77jonas77 <77jonas77@gmail.com>
2012-09-27 09:54:57 +0100

    Corrección
    el metodo sample no funcionaba así que utilicé el método random para
    hacer que escogiera un número aleatorio.

--- bin/ppt.rb
+++ bin/ppt.rb
@@ -4,15 +4,19 @@ tirada = opciones.keys
 puts "Escoje uno de #{tirada.join(', ')}: "
 tirada_jugador = gets.chomp.to_sym #to_sym????
 
-raise ScriptError, "Tu debes ejecutar: ''#{$0}'' seguido por ono de los siguientes '#{tirada.join(', ')}'" unless tirada.include? tirada_jugador
+raise ScriptError, "Tu debes ejecutar: ''#{$0}'' seguido por uno de los siguientes '#{tirada.join(', ')}'" unless tirada.include? tirada_jugador
 
-tirada_maquina = tirada.sample #tirada aleatoria
+
+aleatorio = rand(2)
+tirada_maquina = tirada[aleatorio]
+
+#tirada_maquina = tirada.sample #tirada aleatoria
 
 if tirada_jugador == tirada_maquina
     answer = "Empate"
-elsif tirada_jugador == opciones[tirada_maquina]
+    elsif tirada_jugador == opciones[tirada_maquina]
     answer = "Gana la máquina; #{tirada_maquina} gana a #{tirada_jugador}"
-else
+    else
     answer = "Bien hecho. #{tirada_jugador} gana a #{tirada_maquina}"
 end
 puts answer
